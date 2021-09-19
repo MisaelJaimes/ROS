caa9eedc44d4cc372ea26e5c0bdf00a47b0b2ce2
Misael <misael.jaimes@uipamplona.edu.co>
2021-09-19 18:45:05 -0500

    parte subscriptora al topic cmd_vel funcional

--- script/teleopTurtle.py
+++ script/teleopTurtle.py
@@ -2,18 +2,24 @@
 import rospy
 from geometry_msgs.msg import Twist
 from rospy.exceptions import ROSInterruptException
-data = Twist()
+mensaje = Twist()
 
 def llamada(data):
     rospy.loginfo(data.linear.x)
     rospy.loginfo(data.linear.y)
     rospy.loginfo(data.linear.z)
-    
+    mensaje = data
+    mover_tortuga(mensaje)
 
 def teleopTurtle():
     rospy.init_node('teleopTurtle', anonymous = True)
     rospy.Subscriber("/cmd_vel", Twist, llamada)
     rospy.spin()
+
+def mover_tortuga(mensaje):
+    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size= 10)
+    rate = rospy.Rate(10)
+    pub.publish(mensaje)
 
 
 if __name__ == "__main__":
